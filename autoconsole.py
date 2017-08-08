e9d1ea117d568120d234475cb665b84279b36044
David Ko <david@barobo.com>
2017-08-08 14:46:09 -0700

    Some updates

--- autoconsole.py
+++ autoconsole.py
@@ -10,6 +10,44 @@ try:
 except:
     from mainwindow import Ui_MainWindow
 
+try: 
+    from sproutconsole.rpi import i2c as atlasi2c
+except:
+    from rpi import i2c as atlasi2c
+
+class AsyncI2c(atlasi2c.AtlasI2C):
+    def __init__(self, address=99):
+        super().__init__(address)
+        self.poll_task = None
+
+    def poll(self, interval, callback):
+        self.poll_task = asyncio.ensure_future(self.__poll(interval, callback))
+
+    def cancel(self):
+        if self.poll_task:
+            self.poll_task.cancel()
+            self.poll_task = None
+
+    @asyncio.coroutine
+    def __poll(self, interval, callback):
+        # Query the I2C device
+        result = self.query('R')
+        callback(result)
+        while True:
+            yield from asyncio.sleep(interval)
+            result = self.query('R')
+            callback(result)
+
+class Ph(AsyncI2c):
+    def __init__(self):
+        super().__init__(99)
+
+    def poll(self, interval, callback):
+        def cb(res):
+            callback(float(res.split()[2]))
+        AsyncI2c.poll(self, interval, cb)
+
+
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
@@ -23,4 +61,4 @@ def main():
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    main()
+    # main()

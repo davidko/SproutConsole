52ae330e8d58d2ad62bff324eafe00325f3e19bd
David Ko <david@barobo.com>
2017-08-09 13:46:44 -0700

    Added schema file

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS sproutlocker;
+
+create database sproutlocker;
+
+use sproutlocker;
+
+CREATE TABLE log_types(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO log_types (id, name) VALUES
+    (1, 'Ph'),
+    (2, 'EC'),
+    (3, 'text');
+
+CREATE TABLE log_entries (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    log_type BIGINT NOT NULL,
+    FOREIGN KEY (log_type) REFERENCES log_types(id),
+    data FLOAT,
+    text TEXT,
+    PRIMARY KEY(id)
+);
